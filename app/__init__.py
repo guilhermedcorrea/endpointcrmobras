a41ad7dbb854e997ddce55266f78a73ca11cdcf7
guilhermedcorrea <78979736+guilhermedcorrea@users.noreply.github.com>
2022-01-25 17:36:57 -0300

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,21 @@
-from flask import Flask,jsonify,Response
+from flask import Flask,jsonify,Response, render_template,abort, request
 from flask_sqlalchemy import SQLAlchemy
 import flask_excel as excel
 from flask_mail import Mail
 from flask_marshmallow import Marshmallow,Schema,fields
 import pandas as pd
 from flask_restx import Api
+from flask_cors import CORS, cross_origin
+from flask.json import JSONEncoder
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 excel.init_excel(app)
 mail = Mail(app)
 db = SQLAlchemy(app)
-api = Api(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-
-from app.routes import rotas_crm
-from app.routes import rotas_obras_grandes
+from app.routes import rotas_crm,rotas_obras_grandes
